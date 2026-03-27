a970f2861f82da250c0df2ed8b2ca2316df5c549
Luan <97277916+luandhng@users.noreply.github.com>
2026-03-27 02:48:34 -0700

    update

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1>Lovely things start here</h1>
+      <p>Wow you are so cool</p>
+      <p className={"p-4 flex-col bg-amber-400 m-4 text-green-400 flex "}>
+        Wow you are cool
+      </p>
+      <p>I think we can be better than that</p>
+      <p>I think we can be better than that</p>
+    </div>
+  );
+}
